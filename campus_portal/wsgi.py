105bdeb93d64ebec5b8a107133f1e3139d1d7df8
Ebenj95 <167700284+Ebenj95@users.noreply.github.com>
2026-03-21 16:50:21 +0530

    Auto create superuser

--- campus_portal/wsgi.py
+++ campus_portal/wsgi.py
@@ -3,3 +3,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campus_portal.settings')
 application = get_wsgi_application()
+from django.contrib.auth.models import User
+
+def create_superuser():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+
+create_superuser()
